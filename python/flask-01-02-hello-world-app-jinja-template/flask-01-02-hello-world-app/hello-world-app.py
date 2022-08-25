b57551a0297e34bd8f40949c22eaea08b4e5d2b9
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-08-25 13:37:31 +0200

    Update hello-world-app.py

--- python/flask-01-02-hello-world-app-jinja-template/flask-01-02-hello-world-app/hello-world-app.py
+++ python/flask-01-02-hello-world-app-jinja-template/flask-01-02-hello-world-app/hello-world-app.py
@@ -19,5 +19,5 @@ def forth(id):
 
 if __name__ == '__main__':
     # app.run(debug=False)
-   # app.run(debug=True)
- app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
