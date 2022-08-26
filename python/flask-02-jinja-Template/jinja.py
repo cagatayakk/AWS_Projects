3d68d89bbaca784846c50972cded464c3d714754
M. Cagatay Akkiran <cagatayakkiran3757@gmail.com>
2022-08-26 08:56:06 +0200

    updated

--- python/flask-02-jinja-Template/jinja.py
+++ python/flask-02-jinja-Template/jinja.py
@@ -11,9 +11,8 @@ def number():
     x=15
     y=20
     return render_template('body.html', num1=x, num2=y, sum=x+y)
-
     
 if __name__ == '__main__':
-    # app.run(debug=False)
+    #app.run(debug=False)
     # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
