44c5b1005201e2fd637e35cb6207e216f6e387e3
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-09-22 20:40:21 +0200

    Update CLI_Commands.sh

--- Project-102-Roman-Numerals-Converter/CLI/CLI_Commands.sh
+++ Project-102-Roman-Numerals-Converter/CLI/CLI_Commands.sh
@@ -33,3 +33,47 @@ aws ec2 authorize-security-group-ingress \
 #After creating security Groups, We'll create our EC2 which has latest AMI id. to do this, we need to find out latest AMI with AWS system manager (ssm) command
 #This command is to get description of latest AMI ID that we use.
 aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --region us-east-1
+
+#This command is to run querry to get latest AMI ID
+aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 'Parameters[0].[Value]' --output text
+
+#we'll assign this latest AMI id to the LATEST_AMI environmental variable
+LATEST_AMI=$(aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 'Parameters[0].[Value]' --output text)
+
+#Now we can run the instance with CLI command. (Do not forget to create userdata.sh under "/home/ec2-user/" folder before run this command)
+aws ec2 run-instances \
+    --image-id $LATEST_AMI \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name First_Key \
+    --security-groups roman_numbers_sec_grp \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=roman_numbers}]' \
+    --user-data file:///home/ec2-user/userdata.sh
+
+#To see the each instances Ip we'll use describe instance CLI command
+aws ec2 describe-instances --filters "Name=tag:Name,Values=roman_numbers"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
