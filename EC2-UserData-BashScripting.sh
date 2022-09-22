26d1fd669c2b53df0f92ab9aeecd8457e13e17b3
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-09-22 10:27:01 +0200

    Create EC2-UserData-BashScripting.sh

---
+++ EC2-UserData-BashScripting.sh
@@ -0,0 +1,21 @@
+        #!/bin/bash
+        yum update -y
+        yum install python3 -y
+        pip3 install flask
+        cd /home/ec2-user
+        FOLDER="https://raw.githubusercontent.com/cagatayakk/AWS_Projects/main/Project-102-Roman-Numerals-Converter"
+        wget ${FOLDER}/app.py
+        mkdir templates && cd templates
+        wget ${FOLDER}/templates/index.html
+        wget ${FOLDER}/templates/result.html
+        cd ..
+        python3 app.py
+
+
+        # #!/bin/bash
+        # yum update -y
+        # yum install git -y
+        # pip3 install flask
+        # git clone https://github.com/cagatayakk/AWS_Projects.git
+        # cd AWS_Projects/Project-102-Roman-Numerals-Converter
+        # python3 app.py
