660d3cb1ae276d502f5e4e14a233c2accfac3775
M. Cagatay Akkiran <cagatayakkiran3757@gmail.com>
2022-09-22 15:59:35 +0200

    updated

--- Project-102-Roman-Numerals-Converter/userdata.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-yum update -y
-yum install python3 -y
-pip3 install flask
-cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/cagatayakk/AWS_Projects/main/Project-102-Roman-Numerals-Converter/templates/index.html
-wget -P templates https://github.com/cagatayakk/AWS_Projects/blob/main/Project-102-Roman-Numerals-Converter/templates/result.html
-wget https://raw.githubusercontent.com/cagatayakk/AWS_Projects/main/Project-102-Roman-Numerals-Converter/app.py
-python3 app.py
