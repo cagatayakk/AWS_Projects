c342f6d144e9977206d787633d2f4f8d26936b24
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-09-29 11:34:20 +0200

    Create EC2-UserData-SQLite.sh

---
+++ Flask/flask-07-Handling-SQL/EC2-UserData-SQLite.sh
@@ -0,0 +1,9 @@
+yum update -y
+pip3 install flask 
+pip3 install flask-mysql
+pip3 install sqlalchemy
+pip3 install Flask-SQLAlchemy
+yum install git -y
+git clone https://github.com/cagatayakk/AWS_Projects.git
+cd ./AWS_Projects/Flask/flask-07-Handling-SQL
+python3 app-with-sqlite.py
