fa95aead592242b2c56cfafa25655579b7374a7f
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-09-29 11:28:09 +0200

    Update EC2-UserData-MySQL.sh

--- Flask/flask-07-Handling-SQL/EC2-UserData-MySQL.sh
+++ Flask/flask-07-Handling-SQL/EC2-UserData-MySQL.sh
@@ -4,6 +4,9 @@ pip3 install flask-mysql
 pip3 install sqlalchemy
 pip3 install Flask-SQLAlchemy
 yum install git -y
+git clone https://github.com/cagatayakk/AWS_Projects.git
+cd ./AWS_Projects/Flask/flask-07-Handling-SQL
+python3 app-with-mysql.py
 
 
 
